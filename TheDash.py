14119ec6ada169346425f1a24daf8dd87d893420
Jared <47715974+Jared-dareJ@users.noreply.github.com>
2019-02-18 22:12:32 +0000

    Add files via upload

---
+++ TheDash.py
@@ -0,0 +1,16 @@
+import time
+import random
+import sys
+from tkinter import *
+#Questions. MAKESURE THEY'RE DICTIONARY
+Q1 = {"question": "Who was the first democratically elected leader of the Russian Federation?", "a": "A:Boris Johnson", "b": "B:Boris Yeltsin", "c": "C: Mikhail Gorbachev"}
+Q2 = {"question": "What was the first Carry On film ever made?", "a": "A:Carry On Sergeant", "b": "B:Carry On Cleo", "c": "Carry On Doctor"}
+Q3 = {"question": "Why did Tibetans grow long finger nails?", "a": "A:They're attention seekers.", "b": "B:To pick noses efficiently.", "c": "To scratch their back."}
+Q4 = {"question": "Who signed the Magna Carta", "a": "A:Henry III", "b": "B:King John I", "c": "Richard I"}
+Q5 = {"question": "What sport was Fanny Chmelar recognized for competing in?", "a": "A:Long Jump", "b": "B:Show Jumping", "c": "Ski-ing"}
+Q6 = {"question": "What sport was Fanny Chmelar recognized for competing in?", "a": "A:Long Jump", "b": "B:Show Jumping", "c": "Ski-ing"}
+Q7 = {"question": "Which of these North Korean dictators rules first?", "a": "A:Kim Il-Sung", "b": "B:Kim Jong-Un", "c": "Kim Jong-Il"}
+Q8 = {"question": "What was Shaun Ryders Auto-biography called?", "a": "A:Twisting My Plums", "b": "B:Twisting My Melon", "c": "Ski-ing"}
+
+
+

845f483f6239dab89b9638cee60d69ee3d799cb2
Kai Piper <kai9987kai@gmail.com>
2019-02-19 00:55:35 +0000

    Update TheDash.py

--- TheDash.py
+++ TheDash.py
@@ -8,18 +8,17 @@ from tkinter import ttk
 window = Tk()
  
 window.title("Random question generator V1.0")
-Q1 = {"question": "Who was the first democratically elected leader of the Russian Federation?", "a": "A:Boris Johnson", "b": "B:Boris Yeltsin", "c": "C: Mikhail Gorbachev"}
-Q2 = {"question": "What was the first Carry On film ever made?", "a": "A:Carry On Sergeant", "b": "B:Carry On Cleo", "c": "Carry On Doctor"}
-Q3 = {"question": "Why did Tibetans grow long finger nails?", "a": "A:They're attention seekers.", "b": "B:To pick noses efficiently.", "c": "To scratch their back."}
-Q4 = {"question": "Who signed the Magna Carta", "a": "A:Henry III", "b": "B:King John I", "c": "Richard I"}
-Q5 = {"question": "What sport was Fanny Chmelar recognized for competing in?", "a": "A:Long Jump", "b": "B:Show Jumping", "c": "Ski-ing"}
-Q6 = {"question": "What sport was Fanny Chmelar recognized for competing in?", "a": "A:Long Jump", "b": "B:Show Jumping", "c": "Ski-ing"}
-Q7 = {"question": "Which of these North Korean dictators rules first?", "a": "A:Kim Il-Sung", "b": "B:Kim Jong-Un", "c": "Kim Jong-Il"}
-Q8 = {"question": "What was Shaun Ryders Auto-biography called?", "a": "A:Twisting My Plums", "b": "B:Twisting My Melon", "c": "Ski-ing"}
-Q9 = {"question": "What car did elon musk sent into space?", "a": "Audi", "b": "Honda", "c": "Tesla"}
-Q10 = {"question": "What is the UK?""A:City", "b": "Country", "c": "B:State"}
-Q11 = {"question": "Who is better?""A:PewDiePie", "b": "T-Series", "c": "B:I hate them both"}
-Random= [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10 , Q11]
+Q1 = "Question: Who was the first democratically elected leader of the Russian Federation?, A: Boris Johnson, B: Boris Yeltsin, C: Mikhail Gorbachev"
+Q2 = "Question: What was the first Carry On film ever made?, A: Carry On Sergeant, B: Carry On Cleo, C: Carry On Doctor"
+Q3 = "Question: Why did Tibetans grow long finger nails?, A: They're attention seekers., B: To pick noses efficiently., C: To scratch their back."
+Q4 = "Question: Who signed the Magna Carta, A:Henry III, B: B:King John I, C: Richard I"
+Q5 = "Question: What sport was Fanny Chmelar recognized for competing in?, A: Long Jump, B: Show Jumping, C: Ski-ing"
+Q6 = "Question: Which of these North Korean dictators rules first?, A: Kim Il-Sung, B: Kim Jong-Un, C: Kim Jong-Il"
+Q7 = "Question: What was Shaun Ryders Auto-biography called?, A: Twisting My Plums, B: Twisting My Melon, C: Ski-ing"
+Q8 = "Question: What car did elon musk sent into space?, A: Audi, B: Honda, C: Tesla"
+Q9 = "Question: What is the UK? A: City, B: Country, C: B:State"
+Q10 = "Question: Who is better? A:PewDiePie, b: T-Series, c: B:I hate them both"
+Random= [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10]
 def new():
     lbl.configure(text= random.choice(Random))
 def Close():
@@ -32,4 +31,3 @@ window.resizable(False, False)
 window.iconbitmap('favicon.ico')
 window.attributes("-topmost", True)
 window.mainloop()
-
